a54ae0431016243358d22ab72ec4bddbf6c5d252
Himanshu Verma <sushilverma07836@gmail.com>
2024-10-25 17:31:54 +0530

    update links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import CountryLookup from "./CountryLookup";
 
 export default function Footer() {
@@ -8,16 +9,34 @@ export default function Footer() {
       </div>
       <div className="flex flex-col sm:flex-row justify-between items-center px-8 py-3 space-y-7 sm:space-y-0">
         <ul className="flex items-center space-x-6">
-          <li className="link">About</li>
+          {/* <li className="link">About</li>
           <li className="link">Advertising</li>
           <li className="link">Business</li>
-          <li className="link">How Search works</li>
+          <li className="link">How Search works</li> */}
+          <li className="link">
+          <Link href="https://about.google/intl/ALL_in/">About</Link>
+          </li>
+          <li className="link">
+          <Link href="https://ads.google.com/intl/en_in/home/?subid=ww-ww-et-g-awa-a-g_hpafoot1_1!o2&utm_source=google.com&utm_medium=referral&utm_campaign=google_hpafooter&fg=1">Advertising</Link>
+          </li>
+          <li className="link">
+          <Link href="https://www.google.com/intl/en_in/business/">Business</Link>
+          </li>
+          <li className="link">
+          <Link href="https://www.google.com/search/howsearchworks/?fg=1">How Search works</Link>
+          </li>
         </ul>
-
+        
         <ul className="flex items-center space-x-6">
-          <li className="link">Privacy</li>
-          <li className="link">Terms</li>
-          <li className="link">Settings</li>
+          <li className="link">
+          <Link href="https://policies.google.com/privacy?hl=en-IN&fg=1">Privacy</Link>
+          </li>
+          <li className="link">
+          <Link href="https://policies.google.com/terms?hl=en-IN&fg=1">Terms</Link>
+          </li>
+          <li className="link">
+          <Link href="https://www.google.com/preferences?hl=en-IN&fg=1">Settings</Link>
+          </li>
         </ul>
       </div>
     </footer>
--- src/components/HomeHeader.jsx
+++ src/components/HomeHeader.jsx
@@ -12,7 +12,9 @@ export default function HomeHeader() {
           Images
         </Link>
         <TbGridDots className="bg-transparent hover:bg-gray-200 rounded-full text-4xl p-2" />
+        <Link href="https://accounts.google.com/v3/signin/identifier?continue=https%3A%2F%2Fwww.google.com%2F&ec=GAZAmgQ&hl=en&ifkv=AcMMx-ejZeY442uewLene5ide7ng1I5lpZf0dGKhWK6CKtdAMQk6677RWbRKva11C3GabWoAOPABiQ&passive=true&flowName=GlifWebSignIn&flowEntry=ServiceLogin&dsh=S1916352178%3A1729855505648729&ddm=0">
         <button className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-shadow">Sign in</button>
+        </Link>
       </div>
     </header>
   );
